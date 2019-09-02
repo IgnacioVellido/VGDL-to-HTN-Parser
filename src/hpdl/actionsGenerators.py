43c0d8d7cba17338cac3a5b7767e03f24fdecf62
Ignacio Vellido <ignaciove@correo.ugr.es>
2019-09-02 11:47:29 +0200

    Defining actions for SpriteActionsGenerator

--- src/hpdl/actionsGenerators.py
+++ src/hpdl/actionsGenerators.py
@@ -231,29 +231,36 @@ def SpriteActionGenerator():
 
         # Not clear what it does
         # Missile that produces object at a specific ratio
-        if self.sprite_type == "Bomber":            
+        if self.sprite_type == "Bomber":    
+            # actions.append(self.produce(partner))        
+            # actions.append(self.move(direction ??))
             pass
 
         # Follows partner (or avatar ?) object
-        if self.sprite_type == "Chaser":            
+        if self.sprite_type == "Chaser":    
+            # actions.append(self.follow(partner))        
             pass
 
         # Missile that randomly changes direction
-        if self.sprite_type == "ErraticMissile":            
+        if self.sprite_type == "ErraticMissile":     
+            # actions.append(self.move(direction ??))                   
+            # actions.append(self.changeDirection()) - DOESN'T MAKE SENSE TO BE AN ACTION, IT'S RANDOM
             pass
 
         # Try to make the greatest distance with the partner (or avatar) object
-        if self.sprite_type == "Fleeing":            
+        if self.sprite_type == "Fleeing":       
+            # actions.append(self.flee(partner))     
             pass
 
         # Maybe not needed here
         # Object that dissapear after a moment
         if self.sprite_type == "Flicker":
-            # actions.append(self.expand())
+            # actions.append(self.disappear())
             pass
 
         # Object that moves constantly in one direction
-        if self.sprite_type == "Missile":            
+        if self.sprite_type == "Missile":          
+            # actions.append(self.move(direction ??))              
             pass
 
         # Remove, no information found
@@ -264,25 +271,31 @@ def SpriteActionGenerator():
         if self.sprite_type == "PathChaser":            
             pass
 
+        # Maybe not needed here
         # Oriented object that dissapear after a moment
         if self.sprite_type == "OrientedFlicker":            
-            # actions.append(self.expand())
+            # actions.append(self.dissapear())
             pass
 
         # Acts like a Chaser but sometimes it makes a random move
-        if self.sprite_type == "RandomAltChaser":            
+        if self.sprite_type == "RandomAltChaser":  
+            # The same actions that Chaser, random moves don't need to be defined
             pass
         
         # Acts like a Bomber but randomly change direction
         if self.sprite_type == "RandomBomber":            
+            # The same actions that Bomber, random moves don't need to be defined
             pass
 
         # Acts like a Missile but randomly change direction
         if self.sprite_type == "RandomMissile":            
+            # The same actions that Missile, random moves don't need to be defined
             pass
 
         # Chooses randomly an action in each iteration
-        if self.sprite_type == "RandomNPC":            
+        if self.sprite_type == "RandomNPC":         
+            # We can't know wich action will he choose, probably this will be empty  
+            # In case we want to add something, we should add each action of the NPC 
             pass
 
         # Remove, no information found
@@ -290,15 +303,19 @@ def SpriteActionGenerator():
             pass
 
         # Produces objects following a specific ratio
-        if self.sprite_type == "SpawnPoint":            
+        if self.sprite_type == "SpawnPoint":  
+            # actions.append(self.produce(partner))          
             pass
 
         # Expands in 4 directions if not occupied
-        if self.sprite_type == "Spreader":            
+        if self.sprite_type == "Spreader":       
+            # actions.append(self.expand())     
             pass
 
         # Missile that if when it collides it change to a random direction
-        if self.sprite_type == "Walker":            
+        if self.sprite_type == "Walker":
+            # actions.append(self.move(direction ??))
+            # If collides stop calculating until we know the new direction
             pass
 
         # Not clear what it does
@@ -310,3 +327,82 @@ def SpriteActionGenerator():
 
     # -------------------------------------------------------------------------
     # -------------------------------------------------------------------------
+
+    def move(self, direction):
+        """ Move the object one position in the specified direction """
+        pass
+
+        name = self.sprite_name + "_MOVE"
+        parameters = [["a", self.sprite_type]]        
+        conditions = []
+        effects = []
+
+        return Action(name, parameters, conditions, effects)        
+
+    # -------------------------------------------------------------------------
+
+    def expand(self):
+        """ Expand all objects in the 4 directions if unoccupied """ 
+        # If no other object in that position...
+        pass
+
+        name = self.sprite_name + "_EXPAND"
+        parameters = [["a", self.sprite_type]]        
+        conditions = []
+        effects = []
+
+        return Action(name, parameters, conditions, effects)        
+    
+    # -------------------------------------------------------------------------
+
+    def produce(self, partner):
+        """ Generate an object - IN WICH POSITION ?? """
+        pass
+
+        name = self.sprite_name + "_PRODUCE"
+        parameters = [["a", self.sprite_type], ["p", partner.name]]        
+        conditions = []
+        effects = []
+
+        return Action(name, parameters, conditions, effects)        
+
+    # -------------------------------------------------------------------------
+
+    def dissapear(self):
+        """ Eliminates the object """
+        pass
+
+        name = self.sprite_name + "_DISSAPEAR"
+        parameters = [["a", self.sprite_type]]        
+        conditions = []
+        effects = []
+
+        return Action(name, parameters, conditions, effects)        
+
+    # -------------------------------------------------------------------------
+
+    def chase(self, partner):
+        """ Choose the movement that moves toward the closest sprite of the 
+        partner type """
+        pass
+
+        name = self.sprite_name + "_CHASE"
+        parameters = [["a", self.sprite_type], ["p", partner.name]]        
+        conditions = []
+        effects = []
+
+        return Action(name, parameters, conditions, effects)        
+
+    # -------------------------------------------------------------------------
+
+    def flee(self, partner):
+        """ Choose the movement that moves away the closest sprite of the
+        partner type """        
+        pass
+
+        name = self.sprite_name + "_FLEE"
+        parameters = [["a", self.sprite_type], ["p", partner.name]]        
+        conditions = []
+        effects = []
+
+        return Action(name, parameters, conditions, effects)        
